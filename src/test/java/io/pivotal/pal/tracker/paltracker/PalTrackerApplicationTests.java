524e932b7928ee4d1fc72feae168191593cb3fdd
pu20139798 <punit.bangade@wipro.com>
2020-01-28 09:25:10 -0800

    initial commit

---
+++ src/test/java/io/pivotal/pal/tracker/paltracker/PalTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+//package io.pivotal.pal.tracker.paltracker;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PalTrackerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
